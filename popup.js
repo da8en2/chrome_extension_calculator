17902e8500a2d5a31759c65552ba203e09138610
Daniel Endres <danielendres@gmail.com>
2017-07-02 04:28:30 -0700

    fixed small errors, cleaned code

--- popup.js
+++ popup.js
@@ -1,60 +1,60 @@
 $(function(){
     $('#1').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '1')
     });
     $('#2').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '2')
     });
     $('#3').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '3')
     });
     $('#4').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '4')
     });
     $('#5').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '5')
     });
     $('#6').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '6')
     });
     $('#7').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '7')
     });
     $('#8').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '8')
     });
     $('#9').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       $('#display').val( $('#display').val() + '9')
     });
     $('#0').click(function(){
-      if ( $('#display').val().includes(' ') ) {
+      if ( $('#display').val().includes(' ') || $('#display').val() === '0') {
         $('#display').val('');
       }
       if ( $('#display').val() != '' ) {
@@ -68,60 +68,58 @@ $(function(){
     });
 
     $('#C').click(function(){
-        $('#display').val('')
+        $('#display').val('');
     });
 
     $('#plus').click(function(){
         var storeValue = $('#display').val();
-        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'firstOperand': storeValue});
         chrome.storage.sync.set({'operation': 'add'});
         $('#display').val(storeValue.toString() + ' ');
     });
 
     $('#minus').click(function(){
         var storeValue = $('#display').val();
-        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'firstOperand': storeValue});
         chrome.storage.sync.set({'operation': 'subtract'});
         $('#display').val(storeValue.toString() + ' ');
     });
 
     $('#times').click(function(){
         var storeValue = $('#display').val();
-        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'firstOperand': storeValue});
         chrome.storage.sync.set({'operation': 'multiply'});
         $('#display').val(storeValue.toString() + ' ');
     });
 
     $('#dividedby').click(function(){
         var storeValue = $('#display').val();
-        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'firstOperand': storeValue});
         chrome.storage.sync.set({'operation': 'divide'});
         $('#display').val(storeValue.toString() + ' ');
     });
 
     $('#solve').click(function(){
-      chrome.storage.sync.get(['value1', 'operation'], function(calc){
+      chrome.storage.sync.get(['firstOperand', 'operation'], function(calc){
         var result = 0;
-        var value2 = $('#display').val();
+
+        var secondOperand = $('#display').val();
+
         if (calc.operation == 'add') {
-          if (calc.value1) {
-            result += parseFloat(calc.value1);
-            result += parseFloat(value2);
-          }
+          result = parseFloat(calc.firstOperand) + parseFloat(secondOperand);
         } else if (calc.operation == 'subtract') {
-          if (calc.value1) {
-            result += parseFloat(calc.value1);
-            result -= parseFloat(value2);
-          }
+          result = parseFloat(calc.firstOperand) - parseFloat(secondOperand);
         } else if (calc.operation == 'multiply') {
-          if (calc.value1) {
-            result = parseFloat(value2)*parseFloat(calc.value1);
-          }
+          result = parseFloat(calc.firstOperand) * parseFloat(secondOperand);
         } else if (calc.operation == 'divide') {
-          result = parseFloat(calc.value1)/parseFloat(value2);
+          result = parseFloat(calc.firstOperand) / parseFloat(secondOperand);
+        } else if (calc.operation == 'done') {
+          result = $('#display').val();
         }
-        $('#display').val(result.toString() + ' ');
-        chrome.storage.sync.set({'value1': value2});
+
+        chrome.storage.sync.set({'operation': 'done'});
+
+        $('#display').val(result);
       });
     });
 });
