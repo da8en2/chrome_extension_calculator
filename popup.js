551ca9919ab2e74791f35358b8cd75c093ff0b45
Daniel Endres <danielendres@gmail.com>
2017-07-02 01:03:06 -0700

    implement calculator functions +-*/ and = (only integers for now)

--- popup.js
+++ popup.js
@@ -1,15 +1,3 @@
-// $(function() {
-//   // chrome.storage.sync.get('amount', function(display) {
-//   //   $('#amount').text(display.amount);
-//   // });
-//
-//   $('#1').click(function() {
-//     $('#disp').val('text');
-//   });d
-//
-// });
-
-
 $(function(){
     $('#1').click(function(){
         $('#display').val( $('#display').val() + '1')
@@ -44,5 +32,58 @@ $(function(){
 
     $('#C').click(function(){
         $('#display').val('')
+    });
+
+    $('#plus').click(function(){
+        var storeValue = $('#display').val();
+        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'operation': 'add'});
+        $('#display').val('')
+    });
+
+    $('#minus').click(function(){
+        var storeValue = $('#display').val();
+        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'operation': 'subtract'});
+        $('#display').val('')
+    });
+
+    $('#times').click(function(){
+        var storeValue = $('#display').val();
+        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'operation': 'multiply'});
+        $('#display').val('')
+    });
+
+    $('#dividedby').click(function(){
+        var storeValue = $('#display').val();
+        chrome.storage.sync.set({'value1': storeValue});
+        chrome.storage.sync.set({'operation': 'divide'});
+        $('#display').val('')
+    });
+
+    $('#solve').click(function(){
+      chrome.storage.sync.get(['value1', 'operation'], function(calc){
+        var result = 0;
+        var value2 = $('#display').val();
+        if (calc.operation == 'add') {
+          if (calc.value1) {
+            result += parseInt(calc.value1);
+            result += parseInt(value2);
+          }
+        } else if (calc.operation == 'subtract') {
+          if (calc.value1) {
+            result += parseInt(calc.value1);
+            result -= parseInt(value2);
+          }
+        } else if (calc.operation == 'multiply') {
+          if (calc.value1) {
+            result = parseInt(value2)*parseInt(calc.value1);
+          }
+        } else if (calc.operation == 'divide') {
+          result = parseInt(calc.value1)/parseInt(value2);
+        }
+        $('#display').val(result);
+      });
     });
 });
